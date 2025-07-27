6e4fbf2e447defc667b6d225c78deebabd0275c8
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-07-28 03:26:58 +0600

    logo changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import LOGO from '@/app/Logo/logo.png';
+import logo from "@/app/logo/event-management-logo.png";
 import { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
@@ -17,7 +17,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-blue-400 shadow-md sticky top-0 z-50 h-20"> {/* Increased height to h-20 (80px) */}
       <div className="container mx-auto px-4 py-6 flex items-center justify-between"> {/* Increased py-2 to py-6 (24px padding) */}
         <Link href="/" className="text-2xl font-bold text-white flex items-center">
-          <Image src={LOGO} alt="Event Management System Logo" width={50} height={50} className="mr-2" /> {/* Increased logo size */}
+          <Image src={logo} alt="Event Management System Logo" width={50} height={50} className="mr-2" /> {/* Increased logo size */}
           Event Management
         </Link>
 
