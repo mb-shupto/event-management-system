8da10b90caa7d8e5eb8ce987018e9f8311079ad8
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-07-30 02:51:18 +0600

    component for popup

---
+++ src/components/Popup.js
@@ -0,0 +1,17 @@
+// Popup.js
+import React from 'react';
+import './Popup.css';
+
+const Popup = ({ onClose }) => {
+  return (
+    <div className="popup-container">
+      <div className="popup">
+        <h2>Popup Content</h2>
+        <p>This is the content of the popup.</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
